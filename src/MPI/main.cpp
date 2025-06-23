6470fa5610b0ce6be067fc5787f1e5c3762748ea
Nikhil K <ikhilu95@gmail.com>
2025-06-23 15:11:21 -0400

    Began implementing MPI by using flattened arrays

---
+++ src/MPI/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+#include "finite_difference.H"
+#include "initial_conditions.H"
+#include "misc_functions.H"
+
+std::string zero_pad(int number, int width) {
+    std::ostringstream oss;
+    oss << std::setw(width) << std::setfill('0') << number;
+    return oss.str();
+}
+
+void write_to_file(const std::string& filename, std::vector<double> grid, 
+                   const std::array<int, 2> dims, int halo){
+    std::ofstream fout(filename);
+
+    int nx = dims[0]; // includes halo
+    int ny = dims[1]; // includes halo
+    int curr_index;
+
+    for (int i = halo; i < nx - halo; i++){
+        for (int j = halo; j < ny - halo; j++){
+            curr_index = idx_2d_to_1d(i, j, ny);
+            fout << grid[curr_index] << ", ";
+        }
+        fout << "\n";
+    }
+    fout.close();
+}
+
+int main(int argc, char** argv){
+    // MPI_Init(&argc, &argv);
+
+    // int rank, size;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int nx = 512;
+    int ny = 512;
+    const std::array<int, 2> dims = {nx, ny};
+    int halo = 1;
+
+    double dx = 1/double(nx);
+    double dy = 1/double(ny);
+    double dt = 0.0001;
+    double timesteps = 10000;
+    int dump_freq = 100;
+
+    double alpha = 0.002;
+    double T_hot = 373.0;
+    double T_cold = 273.0;
+
+    check_CFL(alpha, dx, dy, dt);
+
+    std::vector<double> grid_old((nx+2*halo)*(ny+2*halo), 0.);
+    std::vector<double> grid_new((nx+2*halo)*(ny+2*halo), 0.);
+
+    // if (rank == 0){init_hot_square(grid_old, T_hot, T_cold, nx/4, ny/4);}
+    init_hot_square(grid_old, dims, T_hot, T_cold, nx/4, ny/4);
+
+    // MPI stuff
+    // const int local_nx = nx/size; // decompose along x plane
+
+    for(int t = 0; t <= timesteps; t++){
+        boundary_condition_periodic(grid_old, dims, halo);
+
+        if (t%dump_freq == 0){
+            const std::string filename = "T_" + zero_pad(t, 6) + ".txt";
+            write_to_file(filename, grid_old, dims, halo);
+        }
+
+        timestep(grid_old, grid_new, dims, dx, dy, dt, alpha, halo);
+        std::swap(grid_new, grid_old);
+    }
+
+    // MPI_Finalize();
+    return 0;
+}
